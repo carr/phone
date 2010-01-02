5a45301df17c8e8449facb0b5d448658c7fc7cb8
carr <tomislav@infinum.hr>
2010-01-02 05:31:47 +0100

    Fixed everything into english, refactored constructor

--- lib/phone_number.rb
+++ lib/phone_number.rb
@@ -1,23 +1,19 @@
-# Enkapsulira jedan broj telefona za mreže u hrvatskoj i države koje imaju slične formate (slovenija, bosna..)
+# An object representing a phone number for Croatia and countries with similar formats (slovenia, bosnia..)
 # 
-# Sprema broj telefona podijeljen u 3 dijela
-# * country_code - npr. '385', '386'
-# * area_code - npr. '91', '47'
-# * number - npr. '5125486', '451588'
-# 
-# brojevi se interno spremaju bez ikakvih dodatnih interpunkcija i tek pozivom metoda
-# * to_s
-# * format(arg)
-# se formatiraju na određeni način
+# The phone number is recorded in 3 separate parts:
+# * country_code - e.g. '385', '386'
+# * area_code - e.g. '91', '47'
+# * number - e.g. '5125486', '451588'
 #
-# Svi dijelovi su obavezni, ali se country code i area code mogu postaviti preko default_country_code i
-# default_area_code
+# All parts are mandatoriy, but country code and area code can be set for all phone numbers using
+#   PhoneNumber.default_country_code
+#   PhoneNumber.default_area_code
 #
 # Dependencies
-# * koriste se neke .present? i slicne metode koje dodju sa ActiveSupport-om 
+# * ActiveSupport (present?, cattr_accessor)
 #
 # TODO
-# * manualni unos parse formata - recimo da zadajes
+# * manually entering parse format, using regexps for different parts:
 #   :area_code => regexp, :country_code => regexp itd...problem je s "+" karakterom ili "00" recimo
 class PhoneNumber   
   FORMATS = {
@@ -33,37 +29,33 @@ class PhoneNumber
   
   attr_accessor :country_code, :area_code, :number
   
-  # ako parsamo SHORT oblik onda ovo postavi kao country code 
   cattr_accessor :default_country_code
-  # ako parsamo SHORT oblik onda ovo postavi kao area code 
   cattr_accessor :default_area_code  
-  # duljina prvog dijela numbera kod ispisa
+  
+  # length of first number part (using multi number format)
   cattr_accessor :n1_length
-  # defaultna duljina prvog dijela numbera kod ispisa
+  # default length of first number part
   @@n1_length = 3
   
-  # kreira novi broj telefona
   def initialize(*hash_or_args)    
     if hash_or_args.first.is_a?(Hash)
-      hash = hash_or_args.first
-      self.number = hash[:number]
-      self.area_code = hash[:area_code] || self.default_area_code
-      self.country_code = hash[:country_code] || self.default_country_code   
+      hash_or_args = hash_or_args.first
+      keys = {:number => :number, :area_code => :area_code, :country_code => :country_code}
     else
-      self.number = hash_or_args[0]
-      self.area_code = hash_or_args[1] || self.default_area_code
-      self.country_code = hash_or_args[2] || self.default_country_code      
+      keys = {:number => 0, :area_code => 1, :country_code => 2}
     end
     
+    self.number = hash_or_args[ keys[:number] ]
+    self.area_code = hash_or_args[ keys[:area_code] ] || self.default_area_code
+    self.country_code = hash_or_args[ keys[:country_code] ] || self.default_country_code      
+
     raise "Must enter number" if self.number.blank?
     raise "Must enter area code or set default area code" if self.area_code.blank?
     raise "Must enter country code or set default country code" if self.country_code.blank?    
   end
   
-  # kreira phone number objekt iz ulaznog stringa
-  # format se automatski detektira iz stringa pomocu detect_format metode
-  # string se normalizira metodom normalize  
-  # kao rezultat vraca novi PhoneNumber objekt
+  # create a new phone number by parsing a string
+  # the format of the string is detect automatically (from FORMATS)
   def self.parse(string, options={})
     options[:country_code] ||= self.default_country_code
     options[:country_code] ||= self.default_area_code   
@@ -78,12 +70,12 @@ class PhoneNumber
     end
   end
   
-  # da li je string sintaksno ispravan telefonski broj
+  # is this string a valid phone number?
   def self.valid?(string)
     parse(string).present?
   end
   
-  # dijeli string na country_code, area_code i number
+  # split string into hash with keys :country_code, :area_code and :number
   def self.split_to_parts(string, options = {})
     format = detect_format(string)    
     return nil if format.nil?    
@@ -102,7 +94,7 @@ class PhoneNumber
     end    
   end
   
-  # detektira u kojem od podržanih ulaznih formata je ovaj string
+  # detect format (from FORMATS) of input string
   def self.detect_format(string_with_number)
     formats = []
     FORMATS.each_pair do |format, regexp|
@@ -113,34 +105,34 @@ class PhoneNumber
     formats.first
   end
   
-  # iz stringa briše sve znakovi koji nisu brojevi ili znak +
+  # fix string so it's easier to parse, remove extra characters etc.
   def self.normalize(string_with_number)
     string_with_number.gsub /[^0-9+]/, ''
   end
   
-  # ispisuje area code s vodecom nulom, dakle 091 kao 91
+  # format area_code with trailing zero (e.g. 91 as 091)
   def area_code_long
     "0" + area_code if area_code
   end
   
-  # prvih n znamenki broja
+  # first n characters of :number
   def number1
     number[0...self.class.n1_length]
   end
   
-  # ono sto je ostalo nakon number1
+  # everything left from number after the first n characters (see number1)
   def number2
     n2_length = number.size - self.class.n1_length
     number[-n2_length, n2_length]
   end
   
-  # formatira broj telefona u skladu sa zadanim formatom
-  # * %c - country_code (385)
-  # * %a - area_code (91)
-  # * %A - area_code s vodecom nulom (091)
-  # * %n - number (5125486)
-  # * %n1 - prvih @@n1_length znamenki broja (konfigurabilno kroz PhoneNumber.n1_length), defaultno je 3, (512)
-  # * %n2 - ostale znamenke broja (5486)
+  # format phone number
+  # %c - country_code (385)
+  # %a - area_code (91)
+  # %A - area_code with leading zero (091)
+  # %n - number (5125486)
+  # %n1 - first @@n1_length characters of number (configured through PhoneNumber.n1_length), default is 3 (512)
+  # %n2 - last characters of number (5486)
   def format(fmt)    
     fmt.gsub("%c", country_code || "").
            gsub("%a", area_code || "").
@@ -150,17 +142,17 @@ class PhoneNumber
            gsub("%l", number2 || "")                      
   end
   
-  # ispisuje broj telefona. defaultni format je "+%c%a%n"
+  # the default format is "+%c%a%n"
   def to_s
     format("+%c%a%n")
   end
   
-  # da li ovaj broj ima postavljen defaultni country_code
+  # does this number belong to the default country code?
   def has_default_country_code?
     country_code == self.class.default_country_code
   end
   
-  # da li ovaj broj ima postavljen defaultni area_code
+  # does this number belong to the default area code?
   def has_default_area_code?
     area_code == self.class.default_area_code
   end
