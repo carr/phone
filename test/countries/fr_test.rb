06fc3b59d8912efa9c5781d24d48bd0648e00b4e
Vincent Foursov <vincent.foursov@capitainetrain.com>
2011-03-03 17:05:30 +0100

    add 7 code for french mobile

--- test/countries/fr_test.rb
+++ test/countries/fr_test.rb
@@ -10,6 +10,10 @@ class FRTest < Test::Unit::TestCase
   def test_mobile
     parse_test('+33 6 11 22 33 44', '33', '6', '11223344')
   end
+  
+  def test_mobile_07
+     parse_test('+33 7 11 22 33 44', '33', '7', '11223344')
+   end
 
   def test_voip
     parse_test('+33 9 11 22 33 44', '33', '9', '11223344')
