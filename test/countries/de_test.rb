6e67aa5e00d8cb83538813beb63396e9b13d3efd
Tomislav Car <tomislav@infinum.hr>
2011-04-05 20:09:05 +0200

    Added note about germany area codes not working correctly

--- test/countries/de_test.rb
+++ test/countries/de_test.rb
@@ -11,4 +11,9 @@ class DETest < Test::Unit::TestCase
     parse_test('+49 162 3499558', '49', '162', '3499558')
   end  
   
+  # TODO: germany has 2-5 length area codes, that's why this test doesn't go through
+  #def test_country_side
+  #  parse_test('+49 (0)6120 59511-23', '49', '6120', '5951123')
+  #end  
+  
 end
