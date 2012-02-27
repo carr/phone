fbced984830e8c8f71dfaf5231b7e036b817bffa
wmoxam <wesley.moxam@gmail.com>
2012-02-27 14:23:22 -0500

    Fixes German phone parsing

--- test/countries/de_test.rb
+++ test/countries/de_test.rb
@@ -11,9 +11,8 @@ class DETest < Phoner::TestCase
     parse_test('+49 162 3499558', '49', '162', '3499558')
   end
 
-  # TODO: germany has 2-5 length area codes, that's why this test doesn't go through
-  #def test_country_side
-  #  parse_test('+49 (0)6120 59511-23', '49', '6120', '5951123')
-  #end
+  def test_country_side
+    parse_test('+49 (0)6120 59511-23', '49', '6120', '5951123')
+  end
 
 end
