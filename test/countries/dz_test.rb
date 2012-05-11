63bdb3385e896c773b42826047db7e3a76a3ca41
Wesley Moxam <wesley.moxam@gmail.com>
2012-05-11 01:24:24 -0400

    Adds Algeria

---
+++ test/countries/dz_test.rb
@@ -0,0 +1,12 @@
+require File.expand_path(File.dirname(__FILE__) + '/../test_helper')
+
+## Algeria
+class DZTest < Phoner::TestCase
+  def test_local
+    parse_test('+21329123456', '213', "29", '123456', 'Algeria', false)
+  end
+
+  def test_mobile
+    parse_test('+213551234567', '213', '55', '1234567', 'Algeria', true)
+  end
+end
