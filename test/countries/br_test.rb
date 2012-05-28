6f02b0d4e46daf0978cb1cfb14dea7f28fa438a9
Wesley Moxam <wesley.moxam@gmail.com>
2012-05-28 02:40:42 -0400

    Forgot to add these tests

---
+++ test/countries/br_test.rb
@@ -0,0 +1,12 @@
+require File.expand_path(File.dirname(__FILE__) + '/../test_helper')
+
+## Brazil
+class BRTest < Phoner::TestCase
+  def test_local
+    parse_test('+559112345678', '55', "91", '12345678', 'Brazil', false)
+  end
+
+  def test_mobile
+    parse_test('551161234567', '55', '11', '61234567', 'Brazil', true)
+  end
+end
