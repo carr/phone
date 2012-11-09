e79a5846e987aa17f8b64e2448157985c295ae1e
Wesley Moxam <wesley.moxam@gmail.com>
2012-11-09 14:33:11 -0500

    Fixes Egyptian number parsing

--- test/countries/eg_test.rb
+++ test/countries/eg_test.rb
@@ -3,7 +3,7 @@ require File.expand_path(File.dirname(__FILE__) + '/../test_helper')
 ## Egypt
 class EGTest < Phoner::TestCase
   def test_local
-    parse_test('20112345678', '20', '1', '12345678', 'Egypt', true)
     parse_test('20931234567', '20', '93', '1234567', 'Egypt', false)
+    parse_test('201016244087', '20', '1', '016244087', 'Egypt', true)
   end
 end
