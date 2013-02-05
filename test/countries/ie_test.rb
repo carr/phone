f33b62eff483a717815c60ea1bb4c2639904db38
David Ramalho <dramalho@gmail.com>
2013-02-05 12:09:17 +0000

    ...

--- test/countries/ie_test.rb
+++ test/countries/ie_test.rb
@@ -6,11 +6,19 @@ require File.expand_path(File.dirname(__FILE__) + '/../test_helper')
 ## Ireland
 class IETest < Test::Unit::TestCase
 
-  def test_short_cork_number_1
-    parse_test('+353 028 28946', '353', '028', '28946')
+  def test_short_cork_number
+    parse_test('+353 28 28946', '353', '28', '28946')
   end
 
-  def test_short_cork_number_1
-    parse_test('+353 028 28943', '353', '028', '28943')
+  def test_short_cork_number_2
+    parse_test('+353 28 28943', '353', '28', '28943')
+  end
+
+  def test_short_cork_number_3
+    pp Phoner::Phone.split_to_parts '2828946', :country_code => '353'
+    pn = Phoner::Phone.parse '+353 28 28943'
+    pp pn
+
+    pp Phoner::Phone.new ({:number=>"28946", :area_code=>"28", :country_code=>"353"})
   end
 end
