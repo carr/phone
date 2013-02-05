6ad7b04cf73b4ea318a471ce29a599d820bc309f
David Ramalho <dramalho@gmail.com>
2013-02-05 14:58:59 +0000

    Added 08xx extra area codes

--- test/countries/ie_test.rb
+++ test/countries/ie_test.rb
@@ -14,11 +14,11 @@ class IETest < Test::Unit::TestCase
     parse_test('+353 28 28943', '353', '28', '28943')
   end
 
-  def test_short_cork_number_3
-    pp Phoner::Phone.split_to_parts '2828946', :country_code => '353'
-    pn = Phoner::Phone.parse '+353 28 28943'
-    pp pn
+  # def test_short_cork_number_3
+  #   pp Phoner::Phone.split_to_parts '2828946', :country_code => '353'
+  #   pn = Phoner::Phone.parse '+353 28 28943'
+  #   pp pn
 
-    pp Phoner::Phone.new ({:number=>"28946", :area_code=>"28", :country_code=>"353"})
-  end
+  #   pp Phoner::Phone.new ({:number=>"28946", :area_code=>"28", :country_code=>"353"})
+  # end
 end
